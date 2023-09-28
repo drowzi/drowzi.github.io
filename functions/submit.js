6dcc7825e425723e3ce4abaf3d3242418ad8846b
drowzi <45463676+drowzi@users.noreply.github.com>
2023-09-28 00:40:22 -0400

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -1,13 +1,6 @@
-export async function onRequestPost(context) {
-  try {
-    let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
-  } catch (err) {
-    return new Response('Error parsing JSON content', { status: 400 });
-  }
+
+
+export async function onRequest(context) {
+  const task = await context.env.MESSAGES.put(context.name + " @ " + context.email, context.message);
+  return new Response(task);
 }
